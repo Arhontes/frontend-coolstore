60ec60f12975e79894e1d96a8630fcd776daddd3
alex <arhontes@yandex.ru>
2023-07-23 18:23:43 +0300

    feat: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const twColors = require('tailwindcss/colors')
+
+const colors = {
+	transparent: twColors.transparent,
+	black: '#2E3239',
+	gray: '#CDCDCD',
+	white: twColors.white,
+	primary: '#FF9902',
+	secondary: '#161D25',
+	'bg-color': '#F2F2F5',
+	aqua: '#268697',
+	red: twColors.red[400]
+}
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors,
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
