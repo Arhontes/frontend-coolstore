681f9dc38a4a54ef08c393ed324ed039892dfe9e
alex <arhontes@yandex.ru>
2023-07-24 11:36:20 +0300

    fix: fix utils func

--- src/utils/local-storage.ts
+++ src/utils/local-storage.ts
@@ -1,4 +1,6 @@
 export const getStoreLocal = (name: string) => {
+	const localStorage = window.localStorage
+
 	if (typeof localStorage !== 'undefined') {
 		const ls = localStorage.getItem(name)
 		return ls ? JSON.parse(ls) : null
